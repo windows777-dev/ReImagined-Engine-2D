34e149c5886a0fb50994f3d04d3968453aabb243
windows777-dev <mrcooneywindows7@gmail.com>
2026-08-21 13:13:25 +0100

    Added textbox functionality

--- UI/textbox.py
+++ UI/textbox.py
@@ -15,4 +15,29 @@ class TextBox:
         self.highlighted_colour = highlighted_colour
         self.background_colour = background_colour
         self.smoothness = smoothness
-        self.border_size = border_size 
+        self.border_size = border_size
+        self.activated = False
+        self._internal_text = ""
+    def get_text(self):
+        return self._internal_text # Only do this because I dont trust accessing the variable directly
+
+    def is_hover(self):
+        return pr.check_collision_point_rec(pr.get_mouse_position(), pr.Rectangle(self.x, self.y, self.width, self.height))
+    def is_click(self):
+        self.activated = True
+
+        if not self.is_hover() and pr.is_mouse_button_down(pr.MouseButton.MOUSE_BUTTON_LEFT):
+            self.activated = False
+
+        return self.is_hover() and pr.is_mouse_button_down(pr.MouseButton.MOUSE_BUTTON_LEFT)
+
+    def update(self):
+        if (self.activated):
+            key = pr.get_key_pressed()
+
+            key_name = pr.get_key_name(key)
+            self._internal_text += key_name
+
+            
+            
+            
