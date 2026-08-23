87bca327662147e675a1b684d77a32b7abe87f44
windows777-dev <mrcooneywindows7@gmail.com>
2026-08-23 20:00:31 +0100

    Added Text UI Element

---
+++ UI/text.py
@@ -0,0 +1,15 @@
+import pyray as pr
+from UI.font_helper import *
+
+class Text:
+
+    def __init__(self, x=20, y=20, text="TextLabel", font=FontHelper.GetFontPath("NotoSansGothic-Regular.ttf"), font_size=18, colour=pr.BLACK):
+        self.text = text
+        self.font = FontHelper.LoadFont(font)
+        self.x=x
+        self.y=y
+        self.colour = colour
+        pr.set_texture_filter(self.font.texture, pr.TextureFilter.TEXTURE_FILTER_POINT)
+        self.font_size = font_size
+    def draw(self):
+        pr.draw_text_ex(self.font, self.text, pr.Vector2(self.x,self.y), self.font_size, 1.0, self.colour)
